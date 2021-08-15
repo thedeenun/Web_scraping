cfe4e10fd8c2d01e09c873c2534db6377fa263f2
Adeenun Abdullee <thedeenun@Adeenuns-MacBook-Pro.local>
2021-08-15 22:02:15 +0700

    basic scraping

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 from selenium import webdriver
-import time
-import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import time
+import pandas as pd
+
+# driver.find_element_by_css_selector('#page > div.Container-sc-1it4kq4.fZpcck > div.ActionPane-sc-mi4n8g.eVYXmg > div > button.sc-eCApnc.ciuTw').click()
+# for i in driver.find_elements(By.CSS_SELECTOR, '#body > div:nth-child(2) > div > div.row > div.col-md-6 > div.ejocme-0.jDCUqL > div > div > a'):
+#     try:
+#         name = i.find_element_by_css_selector('h2')
+#         NAME = name.text
+#         name.click()
+#         time.sleep(2)
+#     except NoSuchElementException:
+#         name=" "
+#         pass
+#     address = driver.find_element_by_css_selector('div.rg14')
+#     ADDRESS = address.text
+#     df = df.append({
+#         'The attraction':NAME,
+#         'Address':ADDRESS
+#     }, ignore_index=True)
+#     driver.back()
+#     time.sleep(2)
 
 PATH = './chromedriver'
 driver = webdriver.Chrome(PATH)
-driver.get("https://www.wongnai.com/attractions?regions=5322")
+
+driver.get("https://scholar.google.com/")
 
 df = pd.DataFrame()
-length = len(driver.find_elements(By.CSS_SELECTOR, '#body > div:nth-child(2) > div > div.row > div.col-md-6 > div.ejocme-0.jDCUqL > div > div > a'))
-print(length)
-for i in range(length):
-    try:
-        data = driver.find_element_by_css_selector('#body > div:nth-child(2) > div > div.row > div.col-md-6 > div.ejocme-0.jDCUqL > div > div > a')
-        name = data[i].find_element_by_css_selector('h2')
-        NAME = name.text
-        print(NAME)
-        name.click()
-        time.sleep(2)
-    except NoSuchElementException:
-        name=" "
-        pass
-    address = driver.find_element_by_css_selector('#body > div.content.noSidePadding > div.iowx8b-0.jqBVFb.row > div.iowx8b-3.btqpuE.col-md-8 > div:nth-child(5) > div > div > div > div.ba60x6-1.eTmqOK.mt-8-mWeb > div.text-gray-700 > div > div > span > div')
-    ADDRESS = address.text
-    print(ADDRESS)
-    try:
-        phone = driver.find_element_by_css_selector('#body > div.content.noSidePadding > div.iowx8b-0.jqBVFb.row > div.iowx8b-3.btqpuE.col-md-8 > div:nth-child(5) > div > div > div > div.ba60x6-1.eTmqOK.mt-8-mWeb > div:nth-child(5) > div > div > div > span')
-        PHONE = phone.text.split(":")[-1]
-        print(PHONE)
-    except NoSuchElementException:
-        PHONE=" "
-        pass
-    df = df.append({
-        'The attraction': NAME,
-        'Phone': PHONE,
-        'Address': ADDRESS
-    }, ignore_index=True)
-    driver.back()
-    time.sleep(2)
+#search page
+search_box = driver.find_element_by_css_selector("#gs_hdr_tsi")
+search_box.send_keys("Thammasat University\n")
+tu = driver.find_element_by_css_selector('#gs_res_ccl_mid > div:nth-child(2) > div > a')
+tu.click()
 
-df.to_csv('Yala.csv')
+for i in driver.find_elements(By.CSS_SELECTOR, "div.gs_ai_t"):
+    a = i.find_element_by_css_selector('a')
+    author = a.text
+    b = i.find_element_by_css_selector('div.gs_ai_aff')
+    affliation = b.text
+    df = df.append(
+        {
+            'author': author,
+            'affiliation': affliation
+        }, ignore_index=True
+    )
+
+df.to_csv('author.csv')
